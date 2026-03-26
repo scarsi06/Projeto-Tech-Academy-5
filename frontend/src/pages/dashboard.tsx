a9a4540b301e56e87985109d54cfcc9c63984fe2
scarsi06 <guiscarsi06@gmail.com>
2026-03-26 20:34:21 -0300

    dashboard add

---
+++ frontend/src/pages/dashboard.tsx
@@ -0,0 +1,8 @@
+export default function Dashboard() {
+  return (
+    <div style={{ padding: "2rem" }}>
+      <h1>Área Restrita</h1>
+      <p>Bem-vindo ao painel! Você só vê isso se estiver logado.</p>
+    </div>
+  );
+}
